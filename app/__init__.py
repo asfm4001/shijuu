a9def97f4d57ffc284bb11daf408f22a2d79cfd2
asfm4001 <asfm4001@yahoo.com.tw>
2024-12-18 10:47:23 +0800

    refactor: Application Factory(default=devp/test/prod)
    Do to list:
    1. routes is "not" complete.
    2. html URL is "not" comlete.
    3. login import Error.

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,32 @@
-from flask import Flask
+from flask import Flask, Blueprint
 # from flask_bootstrap import Bootstrap
 from flask_bootstrap import Bootstrap4  # Bootstrap-Flask 仍使用相同套件名
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 # from flask_wtf.csrf import CSRFProtect
-from config import config
+from app.config import config
 
-app = Flask(__name__)
-app.config.from_object(config["test"])
-# csrf = CSRFProtect(app)  # 啟用CSRF保護(非flask form需使用)
-# bootstrap = Bootstrap(app)
-bootstrap = Bootstrap4(app)
-bcrypt = Bcrypt(app)
-login = LoginManager(app)
-db = SQLAlchemy(app)
+bootstrap = Bootstrap4()
+bcrypt = Bcrypt()
+login = LoginManager()
+db = SQLAlchemy()
 
-# login_required 設定
-login.login_view = "signin"
-login.login_message = "You must login to access this page."
-login.login_message_category = "info"
+def create_app(configname):
+    app = Flask(__name__)
+    app.config.from_object(config[configname])
+    # csrf = CSRFProtect(app)  # 啟用CSRF保護(非flask form需使用)
+    # bootstrap = Bootstrap(app)
+    bootstrap = Bootstrap4(app)
+    bcrypt = Bcrypt(app)
+    login = LoginManager(app)
+    db = SQLAlchemy(app)
 
-from app.routes import *
+    # login_required 設定
+    login.login_view = "signin"
+    login.login_message = "You must login to access this page."
+    login.login_message_category = "info"
+
+    # routes
+    
+    return app
